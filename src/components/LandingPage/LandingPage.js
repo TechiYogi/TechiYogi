b0725fe2ee12ce269bcf8f9e5e34e930974dbc35
Aseem Mangla <aseemmanglap@gmail.com>
2021-09-26 19:27:15 +0530

    google oAuth added

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import GoogleLogin from 'react-google-login'
+
+const LandingPage = (props) => {
+    const responseGoogle = (response)=>{
+        console.log(response.profileObj);
+    }
+    return (
+        <div>
+                This is the landing Page.
+            <GoogleLogin
+                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                buttonText="Login with Google"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            /> 
+            </div>
+    )
+}
+
+export default LandingPage
