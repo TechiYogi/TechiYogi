e66a3ef76e0d6510199f2bf03ecd9b2dd927e535
Pawan Puneet <coolbangers1438@gmail.com>
2021-10-20 23:43:26 +0530

    practice animations

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage';
 import Dashboard from './components/Dashboard/Dashboard';
+import About from './About';
 
 function App() {
   return (
     <div className="App">
-      {/* <LandingPage/> */}
-      <Dashboard/>
+      <LandingPage/>
+      {/* <Dashboard/> */}
+    <About />
     </div>
   );
 }
